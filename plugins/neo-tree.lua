031452cff3120eac2a59179aa79df80e9caf8b6b
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-02-26 17:38:26 -0500

    always show inlay hints. open neotree on the right

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,11 +1,17 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      position = "right",
+    },
     filesystem = {
       filtered_items = {
         visible = true,
         hide_dot_files = false,
         hide_gitignored = false,
+        never_show = {
+          ".DS_Store",
+        }
       },
     },
   },
