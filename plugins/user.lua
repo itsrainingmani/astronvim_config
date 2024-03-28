26050f236b0871c37839f97bd3e8db568113be26
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-03-28 15:07:36 -0400

    Add conjure & vim-racket for working with SICP

--- plugins/user.lua
+++ plugins/user.lua
@@ -20,5 +20,34 @@ return {
     config = function()
       require('ts-error-translator').setup()
     end,
-  }
+  },
+  {
+    "Olical/conjure",
+    dependencies = {
+      {
+        "PaterJason/cmp-conjure",
+        config = function()
+          local cmp = require("cmp")
+          local config = cmp.get_config()
+          table.insert(config.sources, {
+            name = "buffer",
+            option = {
+              sources = {
+                {name="conjure"},
+              },
+            },
+          })
+          cmp.setup(config)
+        end,
+      }
+    },
+    config = function(_, opts)
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+    end,
+    init = function()
+      vim.g["conjure#debug"] = true
+    end,
+  },
+  "wlangstroth/vim-racket",
 }
